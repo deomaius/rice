421d01fc9cede32ff7462f477267ade852b5cae5
deomaius <samuel.jj.gosling@gmail.com>
2025-11-01 22:48:46 +0000

    correctly configure cmd position right

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -10,5 +10,5 @@ return {
   {
     "folke/ts-comments.nvim",
     enabled = false,
-  },
+  }
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,58 +1,22 @@
 return {
   "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    -- Set split behavior before noice setup
-    vim.opt.splitright = true
-    vim.opt.splitbelow = true
+  opts = {
+    cmdline = {
+      enabled = true,
+      view = "split",
 
-    require("noice").setup({
-      cmdline = {
-        view = "cmdline",
+      opts = {
+        position = "right",
+        size = 30,
       },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
+    },
+    messages = {
+      enabled = true,
+      view = "split",
+      opts = {
+        position = "right",
+        size = 10,
       },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = false, -- position the cmdline and popupmenu together
-        long_message_to_split = false, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-        cmdline_output_to_split = true,
-      },
-      -- Configure routes to respect split settings
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          opts = { skip = true },
-        },
-      },
-      views = {
-        split = {
-          win_options = {
-            winfixheight = true,
-            winfixwidth = false,
-          },
-        },
-      },
-    })
-  end,
+    },
+  },
 }
